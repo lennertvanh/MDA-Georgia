79372f69f97b25509b9061cb0d2d18a05dc33212
Grégoire <gcorluy@gmail.com>
2023-05-19 22:28:52 +0200

    nicer looking plot in app

--- pages/Noise-level-in-function-dry-rainy-day.py
+++ pages/Noise-level-in-function-dry-rainy-day.py
@@ -108,15 +108,7 @@ fig3.show() """
     ], className="dashboard-tiles")
 ]) """
 
-
-layout = html.Div([
-    html.Div([
-        html.H2("Average noise level per month (standardized data)"),
-        dcc.Graph(id="plot", figure=go.Figure(data=go.Bar(x=months, y=average_noise))),
-    ], style={'width': '80%', 'display': 'inline-block', 'vertical-align': 'top'}),
-
-    html.Div([
-        html.Div([
+""" html.Div([
             html.H3("Average Noise for the Total Year"),
             html.Div(f"{mean_total_data:.2f}", id="total-noise"),
         ], className="tile"),
@@ -129,11 +121,47 @@ layout = html.Div([
         html.Div([
             html.H3("Average Noise on Dry Days"),
             html.Div(f"{mean_dry:.2f}", id="dry-noise"),
-        ], className="tile"),
+        ], className="tile"), """
+
+
+layout = html.Div([
+    html.Div([
+        html.H2("Average noise level per month",style={"font-family": 'Lato', "font-size": "54px", "font-weight": "250","padding":"0", "margin": "0 0 0 100px"}),
+        html.Br(),
+        html.H2("raw data",style={"font-family": 'Lato', "font-size": "54px", "font-weight": "250","padding":"0", "margin": "0 0 0 100px","margin-bottom":"-10px"}),
+        
+        
+        dcc.Graph(id="plot", figure=go.Figure(data=go.Bar(x=months, y=average_noise)),style={"margin-top":"0"}),
+    ], style={'width': '80%', 'display': 'inline-block', 'vertical-align': 'top'}),
+
+
+    
+    html.Div([
+        
         html.Div([
-        html.Button("Total Year", id="total-noise", n_clicks=0),
-        html.Button("Rainy Days", id="rainy-noise", n_clicks=0),
-        html.Button("Dry Days", id="dry-noise", n_clicks=0),
+        html.Button(children=[
+            'Total data',
+            html.Br(),
+            f"{mean_total_data:.2f} dB(A)",
+        ],
+          id="total-noise", n_clicks=0, style={'width': '250px','height':"130px","margin-bottom":"10px","margin-top":"20px","border-radius":"10px","font-size":"40px","color":"white","background":"darkmagenta"}),
+
+        html.Br(),
+
+        html.Button(children=[
+            'Rainy days',
+            html.Br(),
+            f"{mean_rainy:.2f} dB(A)",
+        ]
+        , id="rainy-noise", n_clicks=0, style={'width': '250px','height':"130px","margin-bottom":"10px","border-radius":"10px","font-size":"40px","color":"white","background":"darkmagenta"}),
+
+        html.Br(),
+
+        html.Button(children=[
+            'Dry days',
+            html.Br(),
+            f"{mean_dry:.2f} dB(A)",
+        ], id="dry-noise", n_clicks=0, style={'width': '250px','height':"130px","border-radius":"10px","font-size":"40px","color":"white","background":"darkmagenta"}),
     ], className="button-container", style={'display': 'inline-block', 'vertical-align': 'top', 'margin-left': '20px'}),
     
     ], className="dashboard-tiles", style={'display': 'inline-block', 'vertical-align': 'top', 'margin-left': '20px'}),
@@ -149,12 +177,44 @@ layout = html.Div([
 def update_plot(total_clicks, rainy_clicks, dry_clicks):
     ctx = dash.callback_context
     if not ctx.triggered:
-        return go.Figure(data=go.Bar(x=months, y=average_noise))
+        fig = go.Figure(data=go.Bar(x=months, y=average_noise))
+        fig.update_layout(title=dict(text="Total",x=0.5),xaxis_title="Month", yaxis_title="Average Noise Level",
+                          plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)',
+                          title_font_family="Times New Roman",title_font=dict(size=40),
+                          xaxis=dict(showgrid=True, zeroline=True),
+                          yaxis=dict(showgrid=True, zeroline=True))
+        fig.update_xaxes(color="black",gridwidth=5)
+        fig.update_traces(marker_color='blue')
+        return fig
 
     clicked_button_id = ctx.triggered[0]['prop_id'].split('.')[0]
     if clicked_button_id == "total-noise":
-        return go.Figure(data=go.Bar(x=months, y=average_noise))
+        fig = go.Figure(data=go.Bar(x=months, y=average_noise))
+        fig.update_layout(title=dict(text="Total",x=0.5),xaxis_title="Month", yaxis_title="Average Noise Level",
+                          plot_bgcolor='rgba(0,0,0,0)',
+                          title_font_family="Times New Roman",title_font=dict(size=40),
+                          xaxis=dict(showgrid=True, zeroline=True),
+                          yaxis=dict(showgrid=True, zeroline=True))
+        fig.update_xaxes(color="black",gridwidth=5)
+        fig.update_traces(marker_color='blue')
+        return fig
     elif clicked_button_id == "rainy-noise":
-        return go.Figure(data=go.Bar(x=months, y=average_noise_rainy))
+        fig = go.Figure(data=go.Bar(x=months, y=average_noise_rainy))
+        fig.update_layout(title=dict(text="Rainy",x=0.5),xaxis_title="Month", yaxis_title="Average Noise Level (Rainy)",
+                          plot_bgcolor='rgba(0,0,0,0)',
+                          title_font_family="Times New Roman",title_font=dict(size=40),
+                          xaxis=dict(showgrid=True, zeroline=True),
+                          yaxis=dict(showgrid=True, zeroline=True))
+        fig.update_xaxes(color="black",gridwidth=5)
+        fig.update_traces(marker_color='blue')
+        return fig
     elif clicked_button_id == "dry-noise":
-        return go.Figure(data=go.Bar(x=months, y=average_noise_dry))
+        fig = go.Figure(data=go.Bar(x=months, y=average_noise_dry))
+        fig.update_layout(title = dict(text="Dry",x=0.5),xaxis_title="Month", yaxis_title="Average Noise Level (Dry)",
+                          title_font_family="Times New Roman",title_font=dict(size=40),
+                          plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)',
+                          xaxis=dict(showgrid=True, zeroline=True),
+                          yaxis=dict(showgrid=True, zeroline=True))
+        fig.update_xaxes(color="black",gridwidth=5)
+        fig.update_traces(marker_color='blue')
+        return fig
